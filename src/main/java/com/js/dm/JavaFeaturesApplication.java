aed91cca19ab36c18c9285d29a5facb275981722
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2026-03-19 00:14:22 +0530

    main method

--- src/main/java/com/js/dm/JavaFeaturesApplication.java
+++ src/main/java/com/js/dm/JavaFeaturesApplication.java
@@ -1,15 +1,35 @@
 package com.js.dm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class JavaFeaturesApplication {
+public class JavaFeaturesApplication extends SpringBootServletInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(JavaFeaturesApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaFeaturesApplication.class, args);
+        logger.info("Starting JavaFeaturesApplication...");
+
+        SpringApplication.run(JavaFeaturesApplication.class, args);
 	}
 
+    @Bean
+    public ServletContextInitializer initializer() {
+        return servletContext -> {
+            logger.info("JavaFeaturesApplication started in WAR mode!");
+        };
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JavaFeaturesApplication.class);
+    }
 }
